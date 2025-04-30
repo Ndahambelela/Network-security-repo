9372a8f029b9201b7767da9fadcc5cdcda781b95
Ferrariboy <festushelaoshatipamba@gmail.com>
2025-04-30 09:47:53 +0200

    Initial code commit

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;NamZone | All Rights Reserved | Contact Us: +264814630606</b></span></div>
+</footer>
+    
